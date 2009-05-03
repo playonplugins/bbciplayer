572c1effa619b517f93709cdf5f8c287f1d68ad4
Paul Battley <pbattley@gmail.com>
2009-05-03 20:43:19 +0100

    It's easier to read the flow if the remote lookup code is extracted into separate methods.

--- src/ProgrammeDatabase.cs
+++ src/ProgrammeDatabase.cs
@@ -1,5 +1,6 @@
 namespace Beeb {
 
+  using System;
   using System.IO;
   using System.Net;
   using System.Xml;
@@ -12,31 +13,18 @@ namespace Beeb {
     ////
 
     public
-    ProgrammeDatabase() {
-    }
+    ProgrammeDatabase() {}
 
     ////
 
     public ProgrammeItem
     ProgrammeInformation(string pid) {
       ProgrammeItem programmeItem = (ProgrammeItem)this.programmeInformationCache.Get(pid);
-      if (programmeItem != null) return programmeItem;
 
-      string playlistUrl = "http://www.bbc.co.uk/iplayer/playlist/" + pid;
-
-      XmlDocument doc = new XmlDocument();
-      doc.LoadXml(ReadFromUrl(playlistUrl));
-
-      XmlNamespaceManager ns = new XmlNamespaceManager(doc.NameTable);
-      ns.AddNamespace("pl", "http://bbc.co.uk/2008/emp/playlist");
-
-      XmlNode item = doc.SelectSingleNode("//pl:item[@kind='programme'][descendant::pl:alternate[@id='default']]", ns);
-      if (item == null) return null;
-
-      string duration   = item.Attributes["duration"].Value;
-      string identifier = item.Attributes["identifier"].Value;
-      programmeItem = new ProgrammeItem(identifier, System.Int64.Parse(duration));
-      this.programmeInformationCache.Set(pid, programmeItem);
+      if (programmeItem == null) {
+        programmeItem = RemoteLookUpProgrammeItem(pid);
+        this.programmeInformationCache.Set(pid, programmeItem);
+      }
 
       return programmeItem;
     }
@@ -44,26 +32,11 @@ namespace Beeb {
     public string
     VpidToStreamingUrl(string vpid) {
       string streamingUrl = (string)this.vpidToStreamingUrlCache.Get(vpid);
-      if (streamingUrl != null) return streamingUrl;
 
-      string mediaSelectorUrl = "http://www.bbc.co.uk/mediaselector/4/mtis/stream/" + vpid;
-      XmlDocument doc = new XmlDocument();
-      doc.LoadXml(ReadFromUrl(mediaSelectorUrl));
-
-      XmlNamespaceManager ns = new XmlNamespaceManager(doc.NameTable);
-      ns.AddNamespace("bbc", "http://bbc.co.uk/2008/mp/mediaselection");
-
-      XmlNode entry     = doc.SelectSingleNode("//bbc:media[@encoding='vp6']/bbc:connection", ns);
-      if (entry == null) return null;
-
-      string server     = entry.Attributes["server"].Value;
-      string authString = entry.Attributes["authString"].Value;
-      string identifier = entry.Attributes["identifier"].Value;
-
-      streamingUrl = "rtmp://" + server + "/ondemand?_fcs_vhost=" + server +
-                     "&auth=" + authString + "&aifp=v001&slist=" + identifier + "|" +
-                     "<rtmpMedia version=\"1.0\"><mediaPath>" + identifier + "</mediaPath></rtmpMedia>";
-      this.vpidToStreamingUrlCache.Set(vpid, streamingUrl);
+      if (streamingUrl == null) {
+        streamingUrl = RemoteLookUpStreamingUrl(vpid);
+        this.vpidToStreamingUrlCache.Set(vpid, streamingUrl);
+      }
 
       return streamingUrl;
     }
@@ -77,6 +50,46 @@ namespace Beeb {
       string content     = sr.ReadToEnd();
       sr.Close();
       return content;
+    }
+
+    private string
+    RemoteLookUpStreamingUrl(string vpid) {
+      XmlDocument doc = new XmlDocument();
+      string mediaSelectorUrl = "http://www.bbc.co.uk/mediaselector/4/mtis/stream/" + vpid;
+      doc.LoadXml(ReadFromUrl(mediaSelectorUrl));
+
+      XmlNamespaceManager ns = new XmlNamespaceManager(doc.NameTable);
+      ns.AddNamespace("bbc", "http://bbc.co.uk/2008/mp/mediaselection");
+
+      XmlNode entry     = doc.SelectSingleNode("//bbc:media[@encoding='vp6']/bbc:connection", ns);
+      if (entry == null) return null;
+
+      string server     = entry.Attributes["server"].Value;
+      string authString = entry.Attributes["authString"].Value;
+      string identifier = entry.Attributes["identifier"].Value;
+
+      return "rtmp://" + server + "/ondemand?_fcs_vhost=" + server +
+             "&auth=" + authString + "&aifp=v001&slist=" + identifier + "|" +
+             "<rtmpMedia version=\"1.0\"><mediaPath>" + identifier + "</mediaPath></rtmpMedia>";
+    }
+
+    private ProgrammeItem
+    RemoteLookUpProgrammeItem(string pid) {
+      string playlistUrl = "http://www.bbc.co.uk/iplayer/playlist/" + pid;
+
+      XmlDocument doc = new XmlDocument();
+      doc.LoadXml(ReadFromUrl(playlistUrl));
+
+      XmlNamespaceManager ns = new XmlNamespaceManager(doc.NameTable);
+      ns.AddNamespace("pl", "http://bbc.co.uk/2008/emp/playlist");
+
+      XmlNode item = doc.SelectSingleNode("//pl:item[@kind='programme'][descendant::pl:alternate[@id='default']]", ns);
+      if (item == null) return null;
+
+      string duration   = item.Attributes["duration"].Value;
+      string identifier = item.Attributes["identifier"].Value;
+
+      return new ProgrammeItem(identifier, System.Int64.Parse(duration));
     }
   }
 }
