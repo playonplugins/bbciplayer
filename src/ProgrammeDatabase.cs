065b68d17854f62b25ce01333641c0acfad47ef3
Paul Battley <pbattley@gmail.com>
2009-06-29 21:00:42 +0100

    Commented out the thumbnail resizing as requested.

--- src/ProgrammeDatabase.cs
+++ src/ProgrammeDatabase.cs
@@ -51,8 +51,8 @@ namespace Beeb {
         ProgrammeItem prog = ProgrammeInformation(pid);
 
         // The thumbnail is the only piece of information we can't delegate to the playlist:
-        prog.Thumbnail = entry.SelectSingleNode("atom:link/media:content/media:thumbnail", atomNS).Attributes["url"].Value.
-                         Replace("150_84", "640_360");
+        prog.Thumbnail = entry.SelectSingleNode("atom:link/media:content/media:thumbnail", atomNS).Attributes["url"].Value/*.
+                         Replace("150_84", "640_360")*/;
 
         items.Add(prog);
       }
